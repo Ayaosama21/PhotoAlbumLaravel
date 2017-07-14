615737ffdec44c62ad3e3e7bc85def2603406021
Aleksey <u608110@gmai.com>
2017-07-14 18:46:06 +0300

    Create  Display Albums

--- resources/views/albums/index.blade.php
+++ resources/views/albums/index.blade.php
@@ -1,5 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-    <h3>Index Album</h3>
+
+    <h1>Albums</h1>
+
+  @if(count($albums) > 0)
+    <?php
+      $colcount = count($albums);
+  	  $i = 1;
+    ?>
+    <div id="albums">
+    <div class="container">
+        <div class="row center-align">
+            @foreach($albums as $album)
+                @if($i == $colcount)
+                    <div class='col s4'>
+                    <a href="/albums/{{$album->id}}">
+                        <img class="z-depth-3" src="storage/album_covers/{{$album->cover_image}}" alt="{{$album->name}}">
+                        </a>
+                    <br>
+                    <h5>{{$album->name}}</h5>
+                @else
+                    <div class='col s4'>
+                    <a href="/albums/{{$album->id}}">
+                        <img class="z-depth-3" src="storage/album_covers/{{$album->cover_image}}" alt="{{$album->name}}">
+                    </a>
+                    <br>
+                    <h5>{{$album->name}}</h5>
+                @endif
+                    @if($i % 3 == 0)
+                </div></div><div class="row center-align">
+                    @else
+                    </div>
+                @endif
+                    <?php $i++; ?>
+            @endforeach
+        </div>
+      </div>
+    </div>
+  @else
+    <p>No Albums To Display</p>
+  @endif
+
 @endsection
--- resources/views/inc/messages.blade.php
+++ resources/views/inc/messages.blade.php
@@ -1,19 +1,19 @@
 @if (count($errors) > 0)
     @foreach($errors->all() as $error)
-         <div class="card-panel red lighten-1">
+         <div class="card-panel red lighten-1 white-text">
             {{ $error }} 
         </div>
     @endforeach
 @endif
 
 @if(session('success'))
-    <div class="card-panel green darken-1">
+    <div class="card-panel green darken-1 white-text">
         {{ session('success') }} 
     </div>
 @endif
 
 @if(session('error'))
-    <div class="card-panel red lighten-1">
+    <div class="card-panel red lighten-1 white-text">
         {{ session('error') }} 
     </div>
 @endif
